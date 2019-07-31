4b84180172fc81ab5c34420e1a69575e0f988e4d
Srinivas Ashok Kumar <ashok.srinivas@letsventure.com>
2019-07-31 17:25:15 +0530

    hardware check.

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # Android-Fingerprint
 
 Modern Android mobile phones are equipped with fingerprint sensor for security reasons in this example i will demostrate the comprehensive process to setup and use the fingerprint sensor. 
+
+#### Follow this steps
+
+- Check for the operating system greater then marshmallow. 
+- Check for the hardware availability. 
+- Check for manifest and runtime permissions. 
+- Check for User is using device lock atleast one type of lock mechanism. 
+- Check for atleast one fingerprint is being used. 
+
+
+
--- app/src/main/java/com/androidabcd/ashokslsk/fingerprint/MainActivity.kt
+++ app/src/main/java/com/androidabcd/ashokslsk/fingerprint/MainActivity.kt
@@ -1,12 +1,29 @@
 package com.androidabcd.ashokslsk.fingerprint
 
+import android.content.Context
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
+import androidx.core.hardware.fingerprint.FingerprintManagerCompat
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var fingerprintManagerCompat: FingerprintManagerCompat
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
+
+        // check for : OS version
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            // Check for : hardware
+            fingerprintManagerCompat = FingerprintManagerCompat.from(this)
+
+            if (!fingerprintManagerCompat?.isHardwareDetected){
+                subtitle.text = "Oh! Seems like your device has no fingerprint scanner"
+                }
+            }
+        }
 }
