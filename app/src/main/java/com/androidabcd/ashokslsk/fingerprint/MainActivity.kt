58b7791f05c6830874d4ae860883873574ec1720
Srinivas Ashok Kumar <ashok.srinivas@letsventure.com>
2019-07-31 15:32:57 +0530

    Initial commit

---
+++ app/src/main/java/com/androidabcd/ashokslsk/fingerprint/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.androidabcd.ashokslsk.fingerprint
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
